0f546639b12e18eec665d162a833b7f2e9763ac0
Mark Romero <markromero@marks-mbp.lan>
2019-05-05 01:42:25 -0600

    added new path

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require('express'); 
 const bodyParser = require('body-parser');
-
+const path = require('path'); // Usually moved to the start of file
 const app = express();
+
 app.use( express.static( `${__dirname}/../build` ) );
 app.use(bodyParser.json());
 
 const PORT = 3005;
+
+
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 
 app.listen(PORT, function(PORT) {
     console.log('server started');
